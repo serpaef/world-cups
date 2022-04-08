0a7897278bf3f43d2deefc4e2e03187f14dd1176
Fernando Serpa <serpaef@outlook.com>
2022-04-07 21:05:38 -0300

    feat: Cria método para encontrar copa pelo ano
    na camada de service

--- src/services/CupService.ts
+++ src/services/CupService.ts
@@ -8,6 +8,11 @@ class CupService {
     const cups = await this.cupModel.getAll();
     return cups;
   }
+
+  public async getByYear(year:number): Promise<ICup | null> {
+    const cup = await this.cupModel.getByYear(year);
+    return cup;
+  }
 }
 
 export default CupService;
