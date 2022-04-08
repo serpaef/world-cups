5864cb9f2c175bd8d896ebfe005df3f9925f00c1
Fernando Serpa <serpaef@outlook.com>
2022-04-07 21:07:47 -0300

    feat: Cria método para encontrar copa pelo ano
    camada de controller

--- src/controllers/CupController.ts
+++ src/controllers/CupController.ts
@@ -8,11 +8,24 @@ class CupController {
     try {
       const cups = await this.cupService.getAll();
       return res.status(200).json(cups);
-    } catch({message}) {
-
-      return res.status(500).json({message: message})
+    } catch ({ message }) {
+      return res.status(500).json({ message });
     }
-  }
+  };
+
+  public getByYear = async (req: Request, res: Response): Promise<Response> => {
+    const { year } = req.params;
+    try {
+      const cup = await this.cupService.getByYear(Number(year));
+      if (cup) {
+        return res.status(200).json(cup);
+      } else {
+        return res.status(404).json({ message: 'not found' });
+      }
+    } catch ({ message }) {
+      return res.status(500).json({ message });
+    }
+  };
 }
 
 export default CupController;
